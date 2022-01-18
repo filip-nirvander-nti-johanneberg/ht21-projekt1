49bbfeba016ff8df93a0a81ca2b63bf686879624
filip-nirvander-nti-johanneberg <Filip.nirvander@elev.ga.ntig.se>
2022-01-18 10:05:57 +0100

    s

---
+++ javascript/nyindex.js
@@ -0,0 +1,18 @@
+
+
+const nav = document.querySelector('.navlinks')
+const burger = document.querySelector('.burger')
+
+
+nav.classList.toggle('navActive')
+
+function toggleMeny() {
+
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+}
+
+
+burger.addEventListener('click', toggleMeny)
+nav.addEventListener('click', toggleMeny)
+
